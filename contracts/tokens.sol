89b8d95638a1e8602556bfc496947d7071f0d0a8
yoshis2 <seki.yoshitsugu@gmail.com>
2024-02-21 21:15:04 +0900

    token修正

--- contracts/tokens.sol
+++ contracts/tokens.sol
@@ -5,19 +5,19 @@ pragma solidity >=0.4.22 <0.9.0;
 import "./ERC20.sol";
 
 contract Dai is ERC20 {
-    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) ERC20(_name, _symbol, _totalSupply) {
+  constructor(string memory _name, string memory _symbol, uint256 _totalSupply) ERC20(_name, _symbol, _totalSupply){
 
-    }
+  }
 }
 
 contract Link is ERC20 {
-    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) ERC20(_name, _symbol, _totalSupply) {
-        
-    }
+  constructor(string memory _name, string memory _symbol, uint256 _totalSupply) ERC20(_name, _symbol, _totalSupply){
+    
+  }
 }
 
 contract Comp is ERC20 {
-    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) ERC20(_name, _symbol, _totalSupply) {
-        
-    }
+  constructor(string memory _name, string memory _symbol, uint256 _totalSupply) ERC20(_name, _symbol, _totalSupply){
+    
+  }
 }
