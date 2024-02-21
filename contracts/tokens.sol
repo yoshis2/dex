d4b144d8cc0b70229100ad4662947c84dfd3824c
yoshis2 <seki.yoshitsugu@gmail.com>
2024-02-21 15:40:05 +0900

    first commit

---
+++ contracts/tokens.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+        
+pragma solidity >=0.4.22 <0.9.0;
+
+import "./ERC20.sol";
+
+contract Dai is ERC20 {
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) ERC20(_name, _symbol, _totalSupply) {
+
+    }
+}
+
+contract Link is ERC20 {
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) ERC20(_name, _symbol, _totalSupply) {
+        
+    }
+}
+
+contract Comp is ERC20 {
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) ERC20(_name, _symbol, _totalSupply) {
+        
+    }
+}
